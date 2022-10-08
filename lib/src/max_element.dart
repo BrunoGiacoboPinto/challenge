b029cf4a8e1d82a910b6706f8e9d61f4a904c238
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2022-10-08 12:35:55 -0300

    fix: fix typo and add more instructions

--- lib/src/max_element.dart
+++ lib/src/max_element.dart
@@ -4,18 +4,21 @@
 /// elemento em [data] que esteja entre [start] e [end]. Esses valores devem
 /// indicar o primeiro e último índice válido da lista, iniciando em zero.
 ///
-/// Sua implementação deverá passar em todos os casos de testes definidos pelo arquivo `test/challenge_test.dart`.
+/// Sua implementação deverá passar em todos os casos de teste definidos pelo arquivo `test/challenge_test.dart`.
 /// 
 /// Considerações:
 ///
-/// 1) Não é permitido usar nenhuma instrução de interação da linguagem (ex. while, do/while ou for).
-/// 2) Não é permitido utilizar nenhum método da classe `List` que permita interação (forEach, map, etc)
-/// 3) Não é permitido ordenar a lista passada como parâmetro.
+/// 1) Não é permitido usar nenhuma instrução de iteração da linguagem (ex. while, do/while ou for).
+/// 2) Não é permitido utilizar nenhum método da classe `List` que permita iteração (forEach, map, etc)
+/// 3) Não é permitido ordenar a lista passada como parâmetro. Nem mesmo ordenar uma cópia desse parâmetro.
+/// 4) Não é permitido copiar a lista passada por parâmetro e convertê-la para outro tipo de coleção da linguagem Dart.
 ///
 /// Dica:
 ///
 /// - Use a técnica de construção de algoritmos chamada divisão e conquista para resolver esse problema.
 /// - Atente para não acessar posições inválidas dentro da lista
+///
+/// Soluções que violarem as considerações supracitadas serão consideradas incorretas e não pontuarão.
 ///
 int maxElement(List<int> data, int start, int end) {
   throw UnimplementedError('Você deve implementar essa função');
--- lib/src/palindrome.dart
+++ lib/src/palindrome.dart
@@ -3,13 +3,15 @@
 /// Desafio 2: Preencha a implementação do getter [isPalindrome] da extensão de modo a verificar
 /// se a `String` alvo é ou não um palíndromo.
 ///
-/// Sua implementação deverá passar em todos os casos de testes definidos pelo arquivo `test/challenge_test.dart`.
+/// Sua implementação deverá passar em todos os casos de teste definidos pelo arquivo `test/challenge_test.dart`.
 /// 
 /// Considerações:
 ///
 /// - Tente criar uma implementação que privilegie performance do ponto de vista de execução.
 ///
 /// Para mais detalhes sobre o que são palíndromos, leia https://pt.wikipedia.org/wiki/Pal%C3%ADndromo.
+///
+/// Soluções que violarem as considerações supracitadas serão consideradas incorretas e não pontuarão.
 ///
 extension PalindromeExtension on String {
   bool get isPalindrome {
